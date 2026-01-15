1b68e4ed16a3418e24427d9b772bdd0ade908b27
Swapnil Khare <swapnildroid@users.noreply.github.com>
2026-01-15 18:23:51 +0530

    Basic EXO Player

--- app/src/main/java/com/example/ott/MainActivity.kt
+++ app/src/main/java/com/example/ott/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.example.ott.ui.theme.OTTTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,11 +39,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun PlayerScreen(url: String) {
+    val context = LocalContext.current
+
+    val player = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(url))
+            prepare()
+            playWhenReady = false
+
+        }
+    }
+
+    AndroidView(factory = {
+        PlayerView(context).apply {
+            this.player = player
+        }
+    })
+
+    DisposableEffect(Unit) {
+        onDispose { player.release() }
+    }
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        PlayerScreen("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
